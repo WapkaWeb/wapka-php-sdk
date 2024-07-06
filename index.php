b4651dbc651adb5d4930f74db9255b60e094622e
Jonayed Hossan Gazi <me@zuna.id>
2024-07-07 02:39:30 +0600

    Wapka PHP SDK V1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use Wapkaweb\WapkaPhpSdk\API as API;
+
+$API = new API;
+
+var_dump($API);
+
---
+++ src/API.php
@@ -0,0 +1,11 @@
+<?php
+namespace Wapkaweb\WapkaPhpSdk;
+
+class API {
+
+
+
+
+
+
+}
