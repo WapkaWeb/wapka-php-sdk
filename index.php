d1e91c600f98b145d2ede0a337f09ec7de48a3e5
Jonayed Hossan Gazi <me@zuna.id>
2024-07-07 18:41:17 +0000

    Updated

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
+header('content-type: text/plain');
 
 use Wapkaweb\WapkaPhpSdk\API as API;
 
 $API = new API;
 
 var_dump($API);
-
--- src/API.php
+++ src/API.php
@@ -1,11 +1,21 @@
 <?php
+
 namespace Wapkaweb\WapkaPhpSdk;
 
-class API {
+use \MysqliDb;
 
+$db = new MysqliDb(array(
+    'host' => 'host',
+    'username' => 'username',
+    'password' => 'password',
+    'db' => 'databaseName',
+    'port' => 3306,
+    'prefix' => 'my_',
+    'charset' => 'utf8'
+));
+var_dump($db);
+var_dump($db->getOne("test"));
 
-
-
-
-
+class API
+{
 }
